2584ca61988295e7e6f67abf64a163116d2cdccb
phoon-phoon <sirichai.aiew@gmail.com>
2024-11-22 16:07:08 +0700

    Add config and update template prompt

--- src/services/localAI.ts
+++ src/services/localAI.ts
@@ -1,12 +1,31 @@
+import * as vscode from 'vscode'
 export class LocalAIGenerator {
     public template: string
 
     constructor(public prompt: string = '') {
-        this.template = "You are a professional assistant tasked with generating a concise daily report. Use the following format:\n\n### **Daily Report**\n**Date:** [Insert Date]\n\n**1. Completed Tasks**\n[Insert 2-3 bullet points summarizing completed tasks]\n\n**2. In Progress**\n[Insert 2-3 bullet points summarizing ongoing tasks with progress percentages]\n\n**3. Issues**\n[Insert 1-2 bullet points summarizing challenges or blockers]\n\n**4. Tomorrow’s Plan**\n[Insert 2-3 bullet points summarizing planned tasks for the next day]\n\nGenerate the daily report based on this input:\n "
+        this.template = `
+        You are a professional assistant tasked with generating a concise daily report. Use the following format:
+        ### **Daily Report**
+        **Date:** ${new Date().toLocaleDateString('en-US', {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+        })}\n
+        **1. Completed Tasks**\n
+        [Insert 2-3 bullet points summarizing completed tasks]
+        **2. In Progress**\n
+        [Insert 2-3 bullet points summarizing ongoing tasks with progress percentages]\n
+        **3. Issues**
+        [Insert 1-2 bullet points summarizing challenges or blockers]\n
+        **4. Tomorrow's Plan**
+        [Insert 2-3 bullet points summarizing planned tasks for the next day]\n
+        Generate the daily report based on this input:
+        `
     }
 
     async generate() {
-        const response = await fetch('http://192.168.11.76:11434/api/generate', {
+        const config = vscode.workspace.getConfiguration('daily-report')
+        const response = await fetch(`${config.get<string>('aiSummarizationEndpoint') || 'http://localhost:11434'}/api/generate`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
